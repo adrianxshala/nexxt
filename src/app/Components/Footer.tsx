9d0f030d5a7e603de21503406694c91912a55c94
Adrian Shala <adrianshalasuharek@gmail.com>
2025-02-15 18:50:14 +0100

    update

---
+++ src/app/Components/Footer.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      Adrian Shala Footer
+      <Link href="/product">
+        Product Page
+      </Link>{" "}
+    </div>
+  );
+};
+
+export default Footer;
